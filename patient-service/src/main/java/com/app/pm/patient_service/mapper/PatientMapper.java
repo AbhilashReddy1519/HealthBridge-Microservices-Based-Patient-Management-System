106296ce290d8ac05e0d7cb8c423d0799145e8af
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2026-04-16 13:11:05 +0530

    feat: Implement patient service with DTOs and controller for patient management

---
+++ patient-service/src/main/java/com/app/pm/patient_service/mapper/PatientMapper.java
@@ -0,0 +1,36 @@
+package com.app.pm.patient_service.mapper;
+
+import java.time.LocalDate;
+
+import com.app.pm.patient_service.DTO.PatientRequestDTO;
+import com.app.pm.patient_service.DTO.PatientResponseDTO;
+import com.app.pm.patient_service.model.Patient;
+
+import org.springframework.lang.NonNull;
+
+
+
+public class PatientMapper {
+  public static PatientResponseDTO toDTO(Patient patient) {
+    PatientResponseDTO patientDTO = new PatientResponseDTO();
+
+    patientDTO.setId(patient.getId().toString());
+    patientDTO.setName(patient.getName());
+    patientDTO.setAddress(patient.getAddress());
+    patientDTO.setEmail(patient.getEmail());
+    patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+
+    return patientDTO;
+  }
+
+  @NonNull
+  public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+    Patient patient = new Patient();
+    patient.setName(patientRequestDTO.getName());
+    patient.setEmail(patientRequestDTO.getEmail());
+    patient.setAddress(patientRequestDTO.getAddress());
+    patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+
+    return patient;
+  }
+}
